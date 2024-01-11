550142d7815e6b9dafe9ba44f622795eeb788d28
Amrit Adhikari <adhikariamrit222@gmail.com>
2024-01-11 15:45:34 +0545

    commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 144553
+let accountEmail = "ram@gmail.com"
+var accountPassword = "12345"
+accountCity = "Pokhara"
+
+// accountId = 2
+console.log(accountId);
+console.log(accountEmail);
--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,9 @@
-console.log("Ram ")
+let myYoutubename = "hiteshchoudhary"
+let anotherid = myYoutubename
+anotherid = "chaiaur"
+console.log(myYoutubename);
+console.log(anotherid);
+
+const name = "hitesh"
+const repoCount = 50
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
