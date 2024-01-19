80974386527531e571d9222590c0ec05e9e10342
Amrit Adhikari <adhikariamrit222@gmail.com>
2024-01-19 18:17:14 +0545

    Function

---
+++ 01_basics/scope.js
@@ -0,0 +1,23 @@
+
+
+function one(){ 
+    const username = "Hitesh"
+
+    function two(){
+        const website = "Youtube"
+        console.log(username);
+
+    }
+    //console.log(website);
+    two()
+}
+one()
+
+if (true) {
+    const username="Hitesh"
+    if (username==="Hitesh"){
+        const website="Youtube"
+        console.log(username + website);
+    }
+    console.log(username);
+}
